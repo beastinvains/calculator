286f7bb605be277315b86e5fc34d72dc1b36d62c
punit <114797585+beastinvains@users.noreply.github.com>
2022-10-01 19:22:03 +0530

    Add files via upload

---
+++ calculater.py
@@ -0,0 +1,26 @@
+print ("1 = multipal")
+print("2 = divide")
+print("3 = add")
+print ("4 = subctract")
+
+# made by punit sharma
+
+mum = int(input("enter  "))
+def multiply():
+      n1 = int(input("enter num "))
+      n2 = int(input("enter num "))
+      ans = int(n1*n2)
+      print(ans)
+if mum == 1:
+    multiply()
+def divide():
+      n1 = int(input("enter num "))
+      n2 = int(input("enter num "))
+      ans = int(n1/n2)
+      print(ans)
+if mum == 2:
+ divide()
+def add():
+      n1 = int(input("enter num "))
+      n2 = int(input("enter num "))
+
